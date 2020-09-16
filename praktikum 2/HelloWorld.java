900336e7b5bd2d3deee703dc01caefc999dd04ff
riskazataa <71364454+riskazataa@users.noreply.github.com>
2020-09-16 22:59:15 +0700

    HelloWorld.java

---
+++ praktikum 2/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+   public static void main(String[] args) {
+     System.out.println("Nama : Riska Zata Amani");
+     System.out.println("Nim : 6706204062");
+     System.out.println("Kelas : D3IF 44-04");
+   }
+}
